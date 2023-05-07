08260c2d53aa995085f2dd2c9183cd77ab259914
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-05-07 18:22:47 -0300

    remove libx264

--- install.sh
+++ install.sh
@@ -128,7 +128,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libmp3lame \
   --enable-libvorbis \
   --enable-libvpx \
-  --enable-libx264 \
   --enable-nonfree \
   --enable-libsrt && \
 PATH="$HOME/bin:$PATH" make -j$NBCORE && \
