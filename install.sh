ecf8865577ed76d4ffa069dfbb9db3807e5ebc9a
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-05-07 19:00:28 -0300

    update ffmpeg

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-NBCORE=$(cat /proc/cpuinfo | grep processor | wc -l)
-echo cores: $NBCORE
-
 rm -rf ~/ffmpeg_build ~/bin/{ffmpeg,ffprobe,ffplay,x264,x265} ~/ffmpeg_sources
 
 sudo apt-get update -qq && sudo apt-get -y install \
@@ -21,84 +18,83 @@ sudo apt-get update -qq && sudo apt-get -y install \
   libxcb-xfixes0-dev \
   pkg-config \
   texinfo \
+  libdav1d-dev \
+  libopus-dev \
+  libfdk-aac-dev \
   wget \
+  ninja-build \
   zlib1g-dev
 
 mkdir -p ~/ffmpeg_sources ~/bin
 
-# function installLib {
-#   echo installing $1 - flag $2
-#   sudo apt-get install $1
-# }
-
-# installLib nasm 
-
-# sudo apt-get install nasm
-
-
 echo ------------------------------- nasm ----------------------------
 cd ~/ffmpeg_sources && \
-wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2 && \
-tar xjvf nasm-2.14.02.tar.bz2 && \
-cd nasm-2.14.02 && \
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2 && \
+tar xxjvf nasm-2.15.05.tar.bz2 && \
+cd nasm-2.15.05 && \
 ./autogen.sh && \
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
-make -j$NBCORE && \
-make -j$NBCORE install
+make  && \
+make install
 
-echo ------------------------------- yasm ----------------------------
+echo ------------------------------- yasm ---------------------------- modificar
 cd ~/ffmpeg_sources && \
 wget -O yasm-1.3.0.tar.gz https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz && \
 tar xzvf yasm-1.3.0.tar.gz && \
 cd yasm-1.3.0 && \
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
-make -j$NBCORE && \
-make -j$NBCORE install
+make && \
+make install
 
 echo ------------------------------- lib x264 ----------------------------
 cd ~/ffmpeg_sources && \
 git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
 cd x264 && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
-PATH="$HOME/bin:$PATH" make -j$NBCORE && \
-make -j$NBCORE install
+PATH="$HOME/bin:$PATH" 
+make && \
+make install
 
-
-echo ------------------------------- libvpx ----------------------------
+echo ------------------------------- libx265 ---------------------------
+sudo apt-get install libnuma-dev && \
 cd ~/ffmpeg_sources && \
-git -C libvpx pull 2> /dev/null || git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git && \
-cd libvpx && \
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
-PATH="$HOME/bin:$PATH" make -j$NBCORE && \
-make -j$NBCORE install
-
-echo ------------------------------- fdk-aac ----------------------------
-cd ~/ffmpeg_sources && \
-git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
-cd fdk-aac && \
-autoreconf -fiv && \
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
-make -j$NBCORE && \
-make -j$NBCORE install
-
-
-echo ------------------------------- libmp3lame ----------------------------
-cd ~/ffmpeg_sources && \
-wget -O lame-3.100.tar.gz https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz && \
-tar xzvf lame-3.100.tar.gz && \
-cd lame-3.100 && \
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm && \
-PATH="$HOME/bin:$PATH" make -j$NBCORE && \
-make -j$NBCORE install
+wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 && \
+tar xjvf x265.tar.bz2 && \
+cd multicoreware*/build/linux && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source && \
+PATH="$HOME/bin:$PATH" 
+make && \
+make install
 
 echo ------------------------------- libaom ----------------------------
 cd ~/ffmpeg_sources && \
 git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom && \
 mkdir -p aom_build && \
 cd aom_build && \
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom && \
-PATH="$HOME/bin:$PATH" make -j$NBCORE && \
-make -j$NBCORE install
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom && \
+PATH="$HOME/bin:$PATH" 
+make && \
+make install
+
+echo ------------------------------- libsvtav1 --------------------------
+cd ~/ffmpeg_sources && \
+git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
+mkdir -p SVT-AV1/build && \
+cd SVT-AV1/build && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
+PATH="$HOME/bin:$PATH"
+make && \
+make install
+
+echo ------------------------------- libvmaf --------------------------
+cd ~/ffmpeg_sources && \
+wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz && \
+tar xvf v2.1.1.tar.gz && \
+mkdir -p vmaf-2.1.1/libvmaf/build &&\
+cd vmaf-2.1.1/libvmaf/build && \
+meson setup -Denable_tests=false -Denable_docs=false --buildtype=release --default-library=static .. --prefix "$HOME/ffmpeg_build" --bindir="$HOME/bin" --libdir="$HOME/ffmpeg_build/lib" && \
+ninja && \
+ninja install
 
 echo ------------------------------- srt --------------------------------
 sudo apt-get -y install libssl-dev tclsh && \
@@ -107,7 +103,8 @@ git clone https://github.com/Haivision/srt.git && \
 cd srt && \
 # ./configure --prefix="$HOME/ffmpeg_build"  && \
 PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_BINDIR="$HOME/ffmpeg_build/bin" -DCMAKE_INSTALL_INCLUDEDIR="$HOME/ffmpeg_build/include" -DCMAKE_INSTALL_LIBDIR="$HOME/ffmpeg_build/lib" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off
-make -j$NBCORE && make -j$NBCORE install
+make && \ 
+make install
 
 echo ------------------------------- ffmpeg ----------------------------
 cd ~/ffmpeg_sources && \
@@ -120,18 +117,26 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-cflags="-I$HOME/ffmpeg_build/include" \
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
   --extra-libs="-lpthread -lm" \
+  --ld="g++" \
   --bindir="$HOME/bin" \
   --enable-gpl \
+  --enable-gnutls \
+  --enable-libaom \
   --enable-libass \
   --enable-libfdk-aac \
   --enable-libfreetype \
   --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libsvtav1 \
+  --enable-libdav1d \
   --enable-libvorbis \
   --enable-libvpx \
-  --enable-nonfree \
+  --enable-libx264 \
+  --enable-libx265 \
   --enable-libsrt && \
-PATH="$HOME/bin:$PATH" make -j$NBCORE && \
-make -j$NBCORE install && \
+PATH="$HOME/bin:$PATH" 
+make  && \
+make install && \
 hash -r
 
 
