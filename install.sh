a5ee3af4528535d8da06f3392cee9daeaeaae50b
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-05-07 18:21:46 -0300

    remove libaom

--- install.sh
+++ install.sh
@@ -122,7 +122,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-libs="-lpthread -lm" \
   --bindir="$HOME/bin" \
   --enable-gpl \
-  --enable-libaom \
   --enable-libass \
   --enable-libfdk-aac \
   --enable-libfreetype \
