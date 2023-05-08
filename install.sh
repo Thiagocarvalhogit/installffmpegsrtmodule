9cc3386feff83471c8ddf646852ff5720f4bd118
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-05-07 22:37:59 -0300

    disable glp

--- install.sh
+++ install.sh
@@ -114,7 +114,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-libs="-lpthread -lm" \
   --ld="g++" \
   --bindir="$HOME/bin" \
-  --enable-gpl \
   --enable-libaom \
   --enable-libass \
   --enable-libfdk-aac \
