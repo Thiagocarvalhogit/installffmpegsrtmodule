baf7f1614923324008a315b90a3b6a9312cf8669
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-05-13 10:25:12 -0300

    spexx

--- install.sh
+++ install.sh
@@ -130,7 +130,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libfribidi \
   --enable-libfontconfig \
   --enable-libpulse \
-  --enable-libspeex \
   --enable-libsrt
   --enable-libtheora && \
 PATH="$HOME/bin:$PATH" 
