620ffccd3524980864669eca63db4e1c27f1eada
Thiago_Carvalho <94149514+Thiagocarvalhogit@users.noreply.github.com>
2023-08-14 21:41:23 -0300

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-rm -rf ~/ffmpeg_build ~/bin/{ffmpeg,ffprobe,ffplay,x264,x265} ~/ffmpeg_sources
+#!/bin/bash
 
-sudo apt-get update -qq && sudo apt-get -y install \
+# Ensure dependencies are installed (adjust package names based on your system)
+sudo apt-get update -qq
+sudo apt-get -y install \
   autoconf \
   automake \
   build-essential \
@@ -28,71 +30,77 @@ sudo apt-get update -qq && sudo apt-get -y install \
   wget \
   ninja-build \
   zlib1g-dev \
-  libssl-dev
+  libssl-dev \
+  nasm \
+  libbz2-dev
 
+# Make Dir
 mkdir -p ~/ffmpeg_sources ~/bin
 
-echo ------------------------------- nasm ----------------------------
+# Build and install NASM
+echo "----------------------------- nasm ----------------------------"
 cd ~/ffmpeg_sources && \
 wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2 && \
 tar xxjvf nasm-2.15.05.tar.bz2 && \
 cd nasm-2.15.05 && \
 ./autogen.sh && \
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
-make  && \
+make && \
 make install
 
-echo ------------------------------- lib x264 ----------------------------
+# Build and install libx264
+echo "----------------------------- lib x264 ----------------------------"
 cd ~/ffmpeg_sources && \
 git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
 cd x264 && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
-PATH="$HOME/bin:$PATH" 
 make && \
 make install
 
-echo ------------------------------- libx265 ---------------------------
+# Build and install libx265
+echo "----------------------------- libx265 ----------------------------"
 sudo apt-get install libnuma-dev && \
 cd ~/ffmpeg_sources && \
 wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 && \
 tar xjvf x265.tar.bz2 && \
 cd multicoreware*/build/linux && \
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source && \
-PATH="$HOME/bin:$PATH" 
 make && \
 make install
 
-echo ------------------------------- libaom ----------------------------
+# Build and install libaom
+echo "----------------------------- libaom ----------------------------"
 cd ~/ffmpeg_sources && \
 git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom && \
 mkdir -p aom_build && \
 cd aom_build && \
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom && \
-PATH="$HOME/bin:$PATH" 
 make && \
 make install
 
-echo ------------------------------- libsvtav1 --------------------------
+# Build and install libsvtav1
+echo "----------------------------- libsvtav1 --------------------------"
 cd ~/ffmpeg_sources && \
 git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
 mkdir -p SVT-AV1/build && \
 cd SVT-AV1/build && \
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
-PATH="$HOME/bin:$PATH"
 make && \
 make install
 
-echo ------------------------------- srt --------------------------------
+# Build and install srt
+echo "----------------------------- srt --------------------------------"
 sudo apt-get -y install libssl-dev tclsh && \
 cd ~/ffmpeg_sources && \
 git clone https://github.com/Haivision/srt.git && \
 cd srt && \
-# ./configure --prefix="$HOME/ffmpeg_build"  && \
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_BINDIR="$HOME/ffmpeg_build/bin" -DCMAKE_INSTALL_INCLUDEDIR="$HOME/ffmpeg_build/include" -DCMAKE_INSTALL_LIBDIR="$HOME/ffmpeg_build/lib" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off
-make && \ 
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_BINDIR="$HOME/ffmpeg_build/bin" -DCMAKE_INSTALL_INCLUDEDIR="$HOME/ffmpeg_build/include" -DCMAKE_INSTALL_LIBDIR="$HOME/ffmpeg_build/lib" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off && \
+make && \
 make install
 
-echo ------------------------------- ffmpeg ----------------------------
+# Update the environment to include the new binaries
+
+# Navigate to the FFmpeg sources directory and proceed with the installation
 cd ~/ffmpeg_sources && \
 wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
 tar xjvf ffmpeg-snapshot.tar.bz2 && \
@@ -126,9 +134,8 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libfontconfig \
   --enable-libpulse \
   --enable-libsrt && \
-PATH="$HOME/bin:$PATH" 
-make  && \
+make && \
 make install && \
 hash -r
 
-source ~/.profile
+
